0fe0d0ff27e818cddd2c588730cc4e6c200eccd7
Milan Maharjan <milan.com@gmail.com>
2021-03-07 20:56:01 +0545

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const API_KEY = "b60f3577e8eb46f089853e2a9fd7d744";
+const API_KEY = "a474e82615f14a20949ef2f53338569d";
 const CURRENT_WEATHER = `https://api.weatherbit.io/v2.0/current?city=Kiev,UA&key=${API_KEY}`;
 
 export { API_KEY, CURRENT_WEATHER };
